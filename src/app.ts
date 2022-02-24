ac2c3bb9449649d77a503e3ac66926d7b87d331d
Victor Varges <victorryandias@gmail.com>
2022-02-24 20:01:33 -0300

    requirement 1: Creating application in typescript, in order to create a CRUD with REST api. So far, MSC-based files have been created, models with database requests, services implementing all validations, and a controller responsible for responding to the server. Also, I used some features of typescript like the interface: to create the types of my variables, enum: to create messages and fixed statuses, and the typing resources that ts offers. Finally, the creation of a JWT token was implemented as a response if the validation of my users is validated.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import createUser from '../controllers/user';
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/users', createUser);
 
 export default app;
