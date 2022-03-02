84bb893bd59a1902032e9aeed32e93f74d06628b
Victor Varges <victorryandias@gmail.com>
2022-03-02 09:48:03 -0300

    requirement 3: created a structure for the validations of the amount.

--- src/services/product.ts
+++ src/services/product.ts
@@ -7,3 +7,11 @@ const nameProductValidation = (name: string) => {
   if(name.length < 3) return { code:, message: }
 
 };
+
+const amountProductValidation = (amount: string) => {
+
+  if (amount === undefined) return { code:, message: }
+  if (typeof amount !== 'string') { code:, message: }
+  if(amount.length < 3) return { code:, message: }
+
+};
